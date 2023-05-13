c63d7bc2acbfe32904794d66334263c563eb21a2
sieunnnn <wldsmtldsm65@gmail.com>
2023-05-13 15:28:23 +0900

    기본 틀 생성

---
+++ frontend/src/portal/frame/NintendoBottomFrame.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3>bottom frame</h3>
+</template>
+
+<script>
+export default {
+  name: 'NintendoBottomFrame'
+}
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import { userPages } from '@/router/user-pages'
+
+const routes = [
+    ...userPages
+]
+// 라우터 생성
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+// 라우터 추출
+export { router }
