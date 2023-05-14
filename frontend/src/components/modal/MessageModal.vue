1b6cb8b54ae85ba9c80704a0b533b9771251baa1
sieunnnn <wldsmtldsm65@gmail.com>
2023-05-14 17:44:11 +0900

    공통 모달 생성

---
+++ frontend/src/components/modal/MessageModal.vue
@@ -0,0 +1,22 @@
+
+<!-- 대화 창 -->
+<template>
+  <div class="modal_mask">
+    <div>
+      <div class="container_message_modal">
+        <slot name="contents">
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageModal'
+}
+</script>
+
+<style lang="css">
+@import "../../assets/css/modal.css";
+</style>
---
+++ frontend/src/components/modal/ModalFormA.vue
@@ -0,0 +1,22 @@
+
+<!-- 긴 모달 폼 -->
+<template>
+  <div class="modal_mask">
+    <div>
+      <div class="container_modal_form_A">
+        <slot name="contents">
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageModal'
+}
+</script>
+
+<style lang="css">
+@import "../../assets/css/modal.css";
+</style>
