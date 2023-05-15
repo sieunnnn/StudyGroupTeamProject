bd04276edd643d1abbf383c24ab1af6d7155c8c4
sieunnnn <wldsmtldsm65@gmail.com>
2023-05-15 14:50:05 +0900

    데이터 전송 전까지 구현 완료

--- frontend/src/portal/user/UserMain.vue
+++ frontend/src/portal/user/UserMain.vue
@@ -10,18 +10,54 @@
 
     <!-- messageModal -->
     <MessageModal v-if="modalOpen">
+      <template #character>
+        <img src="../../assets/images/npc1_m.png" class="npc1_m"/>
+      </template>
       <template #contents>
         <div :style="`margin: 25px 30px`">
           <div :style="`margin-bottom: 10px`">
             성에 들어가려면 아이디가 있어야해. 아이디를 말해줘
           </div>
-          <div class="select" id="toSignUp" :style="`margin-bottom: 3px`">
+          <div class="select" id="toSignUp" :style="`margin-bottom: 3px`" @click="showSignupModal">
             <img src="../../assets/images/select_btn.png"/>
             아이디..? 없는데...
           </div>
-          <div class="select" id="toLogin">
+          <div class="select" id="toLogin" @click="showLoginModal">
             <img src="../../assets/images/select_btn.png" />
             어디다가 입력하면 되는거야?
+          </div>
+        </div>
+      </template>
+    </MessageModal>
+
+    <!-- messageModal -->
+    <MessageModal v-if="signUpModalOpen">
+      <template #character>
+        <img src="../../assets/images/npc1_m.png" class="npc1_m_2"/>
+      </template>
+      <template #contents>
+        <div :style="`margin: 40px 30px; font-size: 12px`">
+          <div :style="`margin-bottom: 3px`">아래 회원가입 란을 띄어놨으니 얼른 적어줘~</div>
+          <div>빠진거 없는지 꼼꼼히 확인 하는것도 잊지마!</div>
+        </div>
+      </template>
+    </MessageModal>
+
+    <!-- messageModal -->
+    <MessageModal v-if="loginModalOpen">
+      <template #character>
+        <img src="../../assets/images/npc1_m.png" class="npc1_m_2"/>
+      </template>
+      <template #contents>
+        <div :style="`margin: 30px 30px; font-size: 12px`">
+          <div :style="`margin-bottom: 3px`">
+            어쩐지~ 어디서 본 듯 했단 말이지...
+          </div>
+          <div :style="`margin-bottom: 3px`">
+            늘 그랬듯이 아래에다 적어주면 된다구. 엣헴!
+          </div>
+          <div>
+            오늘도 공부 화이팅 하자고! 항상 응원해! 엣헴... 쑥스럽구만.
           </div>
         </div>
       </template>
@@ -41,13 +77,26 @@ export default {
 
   data: function() {
     return {
-      modalOpen: false
+      modalOpen: false,
+      signUpModalOpen: false,
+      loginModalOpen: false
     }
   },
 
   methods: {
     showModal() {
       this.modalOpen = true;
+    },
+
+    showSignupModal() {
+      this.modalOpen = false;
+      this.signUpModalOpen = true;
+    },
+
+    showLoginModal() {
+      this.modalOpen = false;
+      this.signUpModalOpen = false;
+      this.loginModalOpen = true;
     }
   }
 
