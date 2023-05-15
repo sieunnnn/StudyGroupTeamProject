7531412a312076be614f2f1969fffdabf7c8707a
Hyun <87049207+vaynel@users.noreply.github.com>
2023-05-15 17:26:57 +0900

    modal noticeList 페이지 1

--- frontend/src/portal/notice/noticeList.vue
+++ frontend/src/portal/notice/noticeList.vue
@@ -1,5 +1,18 @@
 <template>
-  <noticeLists />
+  <noticeLists>
+    <template #contents>
+      <div class="noticeListHead">
+        공지사항        
+        <div class="noticeLists">
+          1. 공부 열심히 하자! 6월까지 화이팅하자!
+        </div>
+        <hr>
+        <div class="noticeLists">
+          2. 6월달에 SQLD 합격하자!
+        </div>
+      </div>
+    </template>
+  </noticeLists>
 </template>
 
 <script>
@@ -14,5 +27,5 @@ export default {
 </script>
 
 <style>
-
+@import "../../assets/css/noticeList.css";
 </style>
