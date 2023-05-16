effe40cdebffa1f2caa2dc7ceeb9ca2e6e8fbd80
sieunnnn <wldsmtldsm65@gmail.com>
2023-05-17 00:20:06 +0900

    닌텐도 프레임 내용 변경 / 공통 css 내용 변경

--- frontend/src/portal/frame/NintendoTopFrame.vue
+++ frontend/src/portal/frame/NintendoTopFrame.vue
@@ -4,11 +4,13 @@
 
 <script>
 import UserMain from '@/portal/user/UserMain'
+// import UserSignup from '@/portal/user/UserLoginTop'
 
 export default {
   name: 'NintendoTopFrame',
   components: {
     UserMain
+    // UserSignup
   }
 }
 </script>
