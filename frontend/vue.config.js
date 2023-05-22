5a1793b9b9118ac9eff5fc7ce83fc555084fc0cf
sieunnnn <wldsmtldsm65@gmail.com>
2023-05-22 18:53:26 +0900

    cors 문제 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,9 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/',
-        ws:true,
-        changeOrigin: true
+        target: 'http://localhost:8080/'
       }
     }
   },
